ae7cbca717fe196711c83350e84d5f2a01611082
kaltsegayemihretu <kaltsegayemihretu@gmail.com>
2023-12-26 13:02:04 +0300

    Add project files.

---
+++ ATM_Final/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ATM_Final
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(1218, 669);
+            this.Name = "Form1";
+            this.ResumeLayout(false);
+
+        }
+    }
+}
